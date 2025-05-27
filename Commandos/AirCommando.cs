aa6ad891d65c948451864878c454d5ca23b37e11
shmuelgross <shmuelgross3@gmail.com>
2025-05-27 12:49:46 +0300

    create function to status attack for each type commando

--- Commandos/AirCommando.cs
+++ Commandos/AirCommando.cs
@@ -9,7 +9,7 @@ namespace Commandos
     internal class AirCommando : Commando
     {
         // constructor
-        public AirCommando (string name, string codeName, string[] tools): base(name, codeName, tools)
+        public AirCommando(string name, string codeName, string[] tools) : base(name, codeName, tools)
         {
         }
 
@@ -17,6 +17,12 @@ namespace Commandos
         public void Parachute()
         {
             Console.WriteLine($"Commando {this.MyCodeName} is parachuting.");
+        }
+
+        // override getStatusAttack method
+        public override void getStatusAttack()
+        {
+            Console.WriteLine($"Air Commando with codename {this.MyCodeName} is attacking from the air.");
         }
     }
 }
